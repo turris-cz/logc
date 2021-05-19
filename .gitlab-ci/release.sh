fc883cf461a1a817195d4bfdc874f24c6424bd5e
Karel Kočí <karel.koci@nic.cz>
2021-05-19 22:05:11 +0200

    gitlab-ci: fix invalid name of archives in release script
    This was just copy of file from base64c library and i forget to change
    it.

--- .gitlab-ci/release.sh
+++ .gitlab-ci/release.sh
@@ -4,8 +4,8 @@ set -eu
 VERSION="$(echo "${CI_COMMIT_TAG}" | sed -nE 's/v([0-9]+)\.([0-9]+)\.([0-9]+).*/\1.\2.\3/p')"
 
 declare -a args
-for dist in base64c-*.tar.gz base64c-*.tar.xz base64c-*.zip; do
-	URL="${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/generic/base64c/${VERSION}/${dist}"
+for dist in logc-*.tar.gz logc-*.tar.xz logc-*.zip; do
+	URL="${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/generic/logc/${VERSION}/${dist}"
 	curl --header "JOB-TOKEN: ${CI_JOB_TOKEN}" --upload-file "${dist}" "${URL}"
 	args+=("--assets-link" "{\"name\":\"${dist}\",\"url\":\"${URL}\"}")
 done
